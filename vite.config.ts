885249a7e30e53190ecab82238c1bf037954a0fb
Matheus Cavalcanti <matheusc.albqq@gmail.com>
2025-08-01 18:25:23 -0300

    mudança de nome repo

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:'/renob-site-test/',
+  base:'/renob/',
   server: {
     host: "::",
     port: 8080,
