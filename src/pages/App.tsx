529cafaffa4b6a88f50ba3b4af7238be60ef2787
Matheus Cavalcanti <matheusc.albqq@gmail.com>
2025-08-04 09:17:47 -0300

    base_url att

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
           <div className="min-h-screen bg-background">
           <Navigation />
           <Routes>
